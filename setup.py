7b639bfe8d021f49719882d58e671573ee501af0
Benoit Germonpre <benoit.germonpre@4cconsulting.com>
2018-03-17 17:19:40 +0100

    Modify setup

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='mlengine-boilerplate',
+    name='mlengine-mnist',
     version='0.1',
     author='Benoit Germonpre',
     author_email='benoitgermonpre@gmail.com',
-    install_requires=['tensorflow==1.6.0'],
-    packages=find_packages(exclude=['data', 'predictions']),
+    install_requires=['tensorflow==1.6.0', 'Pillow==5.0.0', 'numpy==1.14.0'],
+    packages=find_packages(),
+    scripts=[],
+    package_data={
+        'trainer': ['*'],  # include any none python files in trainer
+    },
     description='ML Engine boilerplate code',
     url='https://github.com/BenoitGermonpre/MNIST_test'
 )
