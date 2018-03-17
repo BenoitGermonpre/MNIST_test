95a14519aec997d4b266d5f091e8f01d7c5b56c7
Benoit Germonpre <benoit.germonpre@4cconsulting.com>
2018-03-17 14:35:46 +0100

    First version

--- trainer/config.py
+++ trainer/config.py
@@ -1,17 +1,12 @@
 PROJECT_ID = 'gcp-workshop-ml6'
-#BUCKET = 'gs://bg_mnist_test/'
-#DATA_DIR = BUCKET + 'data/'
-#TFRECORD_DIR = BUCKET + 'tfrecords/'
-#MODEL_DIR = BUCKET + 'model/'
-#BATCH_SIZE = 100
-
-
-
-PATH = "C:/Users/Benoit Germonpre/Google Drive/4C Documents/PycharmProjects/MNIST_Google_demo/"
-TFRECORD_DIR = PATH + "mnist_tfrecords/"
-MODEL_DIR = PATH + 'model/'
-DATA_DIR = "C:/Users/Benoit Germonpre/Desktop"
+BUCKET = 'gs://bg_mnist_test/'
+DATA_DIR = BUCKET + 'data/'
+TFRECORD_DIR = BUCKET + 'tfrecords/'
+MODEL_DIR = BUCKET + 'model/'
 BATCH_SIZE = 100
 NUM_LABELS = 10
 WIDTH = 28
 HEIGHT = 28
+
+
+
