5830bd71c06a9eff215c6104232cc1b565cd54a8
Benoit Germonpre <benoit.germonpre@4cconsulting.com>
2018-03-18 14:26:10 +0100

    __init__.py in trainer

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     version='0.1',
     author='Benoit Germonpre',
     author_email='benoitgermonpre@gmail.com',
-    install_requires=['tensorflow==1.6.0', 'Pillow==5.0.0', 'numpy==1.14.0'],
+    install_requires=['tensorflow==1.4.0', 'numpy==1.14.0'],
     packages=find_packages(),
     scripts=[],
     package_data={
         'trainer': ['*'],  # include any none python files in trainer
     },
-    description='ML Engine boilerplate code',
+    description='ML Engine MNIST code',
     url='https://github.com/BenoitGermonpre/MNIST_test'
 )
